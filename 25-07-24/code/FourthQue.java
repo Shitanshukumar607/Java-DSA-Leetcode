9ff349d02b03903a6eb6598b0692f1e083b39f01
Shitanshu <shitanshukumar639@gmail.com>
2024-07-25 23:04:01 +0530

    23:03

---
+++ 25-07-24/code/FourthQue.java
@@ -0,0 +1,28 @@
+// ******
+// ****
+// ***
+// **
+// *
+
+
+
+public class FourthQue {
+    public static void main(String[] args) {
+
+        int n = 4;
+      
+        for(int i = n ; i >= 0; i--) {
+        
+        
+        System.out.print("*");
+
+        for(int j = 1; j <= i ; j++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+       }
+
+    }
+}
---
+++ 25-07-24/summary.md
@@ -0,0 +1,9 @@
+## What I Did
+
+- only solved a few pattern Questions from the video
+
+## Questions I solved
+
+- [Inverted Half Pyramid](./code/FourQue.java)
+- [Mirrored Half Pyramid](./code/FifthQue.java)
+
